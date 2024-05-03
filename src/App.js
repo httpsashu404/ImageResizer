79d2e7572f44ea8df9596f6f771bc989b1f7f480
Ashutosh Kumar <159816902+httpsashu404@users.noreply.github.com>
2024-05-03 14:38:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import ImageResizer from "./Componets/ImageResizer";
+
+function App() {
+  return (
+    <div className="App">
+      <ImageResizer />
+    </div>
+  );
+}
+
+export default App;
+
